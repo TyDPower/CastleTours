4d65373eb079c8e1f7f5f28b78daff3f81fec08d
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2022-02-18 22:41:24 +0000

    Update TicketOrderService
    Moved the calculating of the ticket order from the dialog to the TicketOrderService.
    Moved the opening and closing of the ticketDialog from DialogState to TicketOrderState, so that the ticket can be cleared when the dialog is cancelled.

--- Client/Services/TicketOrderService/ITicketOrderService.cs
+++ Client/Services/TicketOrderService/ITicketOrderService.cs
@@ -4,9 +4,15 @@ namespace CastleTours.Client.Services.TicketOrderService
 {
     public interface ITicketOrderService
     {
-        void CreateTicketOrder(int qty, decimal ticketCost, string tourName,
+        bool GetTicketDialogStatus();
+        void ShowTicketDialog();
+        void CloseTicketDialog();
+        void CreateTicketOrder(/*int qty,*/ decimal ticketCost, string tourName,
             string castleName, string location, List<Addon> tourAddons);
         Ticket GetTicketOrder();
+        string GetTicketOrderTotalFormatted();
+        void SetTicketQty(int ticketQty);
+        void SetTicketOrderTotal(decimal selectedTourCost, List<Addon> selectedAddons);
         Task ClearTicketOrder();
     }
 }
--- Client/Services/TicketOrderService/TicketOrderService.cs
+++ Client/Services/TicketOrderService/TicketOrderService.cs
@@ -4,13 +4,16 @@ namespace CastleTours.Client.Services.TicketOrderService
 {
     public class TicketOrderService : ITicketOrderService
     {
-        public Ticket TicketOrder { get; private set; }
+        public Ticket TicketOrder { get; private set; } = new Ticket(); //change to private
+        public bool TicketDialog { get; private set; } = false;
 
-        public void CreateTicketOrder(int qty, decimal ticketCost, string tourName, 
+        public event Action OnChange;
+
+        public void CreateTicketOrder(/*int qty, */decimal ticketCost, string tourName, 
             string castleName, string location, List<Addon> tourAddons)
         {
-            TicketOrder = new Ticket();
-            TicketOrder.Qty = qty;
+            //TicketOrder = new Ticket();
+            //TicketOrder.Qty = qty;
             TicketOrder.TicketCost = ticketCost;
             TicketOrder.TourName = tourName;
             TicketOrder.CastleName = castleName;
@@ -18,14 +21,53 @@ namespace CastleTours.Client.Services.TicketOrderService
             TicketOrder.TourAddons = tourAddons;
         }
 
+        public bool GetTicketDialogStatus()
+        {
+            return TicketDialog;
+        }
+
+        public void ShowTicketDialog()
+        {
+            TicketDialog = true;
+        }
+
+        public void CloseTicketDialog()
+        {
+            TicketDialog = false;
+            TicketOrder = new Ticket();
+        }
+
         public Ticket GetTicketOrder()
         {
             return TicketOrder;
         }
 
+        public string GetTicketOrderTotalFormatted()
+        {
+            return TicketOrder.TicketCost.ToString("0.00");
+        }
+
+        public void SetTicketQty(int ticketQty)
+        {
+            TicketOrder.Qty = ticketQty;
+        }
+
+        public void SetTicketOrderTotal(decimal selectedTourCost, List<Addon> selectedAddons)
+        {
+            decimal addonsTotal = 0.00m;
+            decimal tourCost = selectedTourCost;
+            int ticketQty = TicketOrder.Qty;
+
+            foreach (var i in selectedAddons) addonsTotal += i.Price;
+            decimal ticketCost = (tourCost + addonsTotal) * ticketQty;
+
+            TicketOrder.TicketCost = ticketCost;
+        }
+
         public async Task ClearTicketOrder()
         {
-            TicketOrder = null;
+            TicketOrder = new Ticket();
         }
+
     }
 }
