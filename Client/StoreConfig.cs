b3f801b9bbcddbb354583e72664f34868c9802cf
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2022-02-19 00:47:00 +0000

    Code refactor

--- Client/StoreConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CastleTours.Client
-{
-    public class StoreConfig
-    {
-        public int MaxTicketQty { get; private set; } = 10;
-        public bool BuyTicketWithoutLoggedIn { get; private set; } = true;
-    }
-}
