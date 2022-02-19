5603a63481efc2f1d23ea7460a52d334817b5875
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2022-02-19 08:51:51 +0000

    Create CartItem.cs
    Created CartItem Modal for cart to handle the transactions

---
+++ Shared/Models/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastleTours.Shared.Models
+{
+    public class CartItem
+    {
+        public Tour SelectedTour { get; set; }
+        public Castle Castle { get; set; }
+        public List<Addon> SelectedAddons { get; set; } = new List<Addon>();
+    }
+}
