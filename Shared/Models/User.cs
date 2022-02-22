6c7a9bc3a76920cff4730e8dcb3720c10be91e27
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2022-02-22 23:08:23 +0000

    Create User.cs
    Created user modal for database

---
+++ Shared/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastleTours.Shared.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string MobileNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Bio { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public bool RecieveNewsletters { get; set; } = false;
+        public bool ReadTsAndCs { get; set; } = false;
+        public DateTime DateOfBirth { get; set; } = DateTime.Now;
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public bool IsDeleted { get; set; } = false;
+        public bool IsConfirmed { get; set; } = false;
+    }
+}
