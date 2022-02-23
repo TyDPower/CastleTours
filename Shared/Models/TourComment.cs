2f789ed58fd2011b147effbc5f8850fb253b393c
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2022-02-23 15:24:05 +0000

    Update TourComment.cs

--- Shared/Models/TourComment.cs
+++ Shared/Models/TourComment.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CastleTours.Shared.Models
@@ -9,6 +10,7 @@ namespace CastleTours.Shared.Models
     public class TourComment : Comment
     {
         public int TourId { get; set; }
+        [JsonIgnore]
         public Tour Tour { get; set; }
         public int Rating { get; set; }
     }
