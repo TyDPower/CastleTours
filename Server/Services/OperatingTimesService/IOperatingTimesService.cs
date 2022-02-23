5fb8156fefb600f436de2b808ed82619f400812a
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2022-02-23 15:17:47 +0000

    OperatingTimesService Created
    Created operating times service, however may not be used as the tour service can grab all the needed information

---
+++ Server/Services/OperatingTimesService/IOperatingTimesService.cs
@@ -0,0 +1,11 @@
+﻿using CastleTours.Shared.Models;
+
+namespace CastleTours.Server.Services.OperatingTimesService
+{
+    public interface IOperatingTimesService
+    {
+        Task<List<OperatingTime>> GetOperatingTimesByCastleId(int castleId);
+
+        Task<List<OperatingTime>> GetOperatingTimesByTourId(int tourId);
+    }
+}
---
+++ Server/Services/OperatingTimesService/OperatingTimesService.cs
@@ -0,0 +1,26 @@
+﻿using CastleTours.Server.Data;
+using CastleTours.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CastleTours.Server.Services.OperatingTimesService
+{
+    public class OperatingTimesService : IOperatingTimesService
+    {
+        private readonly DataContext Context;
+
+        public OperatingTimesService(DataContext context)
+        {
+            Context = context;
+        }
+
+        public async Task<List<OperatingTime>> GetOperatingTimesByCastleId(int castleId)
+        {
+            return await Context.OperatingTimes.Where(ot => ot.CastleId == castleId).ToListAsync();
+        }
+
+        public async Task<List<OperatingTime>> GetOperatingTimesByTourId(int tourId)
+        {
+            return await Context.OperatingTimes.Where(ot => ot.TourId == tourId).ToListAsync();
+        }
+    }
+}
