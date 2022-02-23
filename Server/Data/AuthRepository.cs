295d251951b285d63d2004dfa17c0b877b87bb09
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2022-02-23 08:04:53 +0000

    Update AuthRepository.cs
    Server side:
    Added method to create token on login.
    Expires in 24hr.

--- Server/Data/AuthRepository.cs
+++ Server/Data/AuthRepository.cs
@@ -1,5 +1,8 @@
 ﻿using CastleTours.Shared.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 
 namespace CastleTours.Server.Data
@@ -7,10 +10,12 @@ namespace CastleTours.Server.Data
     public class AuthRepository : IAuthRepository
     {
         private readonly DataContext _context;
+        private readonly IConfiguration _configuration;
 
-        public AuthRepository(DataContext context)
+        public AuthRepository(DataContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
         public async Task<ServiceResponse<string>> Login(string email, string password)
         {
@@ -23,7 +28,7 @@ namespace CastleTours.Server.Data
             }
             else
             {
-                response.Data = user.Id.ToString();
+                response.Data = CreateToken(user);
             }
 
             return response;
@@ -81,6 +86,29 @@ namespace CastleTours.Server.Data
 
                 return true;
             }
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Email)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
         }
     }
 }
