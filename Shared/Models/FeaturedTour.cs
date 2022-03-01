c33a6a5bf4cd07c69dbc4034058f59e441575c9a
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2022-03-01 14:17:30 +0000

    Create FeaturedTour.cs
    FeaturedTour modal

---
+++ Shared/Models/FeaturedTour.cs
@@ -0,0 +1,15 @@
+﻿namespace CastleTours.Shared.Models
+{
+    public class FeaturedTour
+    {
+        public int TourId { get; set; }
+        public string TourName { get; set; }
+        public string TourImageUrl { get; set; }
+        public string TourBlurb { get; set; }
+        public string TourBasePrice { get; set; }
+        public int TourRating { get; set; }
+        public int CastleId { get; set; }
+        public string CastleName { get; set; }
+        public string ExperaincesBasePrice { get; set; }
+    }
+}
