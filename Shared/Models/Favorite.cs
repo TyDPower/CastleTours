5de4f8d967424bea658a341775b71f73f07b508c
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2022-03-03 21:54:38 +0000

    Create Favorite.cs
    Modal for adding favorites to user profile

---
+++ Shared/Models/Favorite.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastleTours.Shared.Models
+{
+    public class Favorite
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int TourId { get; set; }
+        public DateTime DateFavorited { get; set; }
+    }
+}
