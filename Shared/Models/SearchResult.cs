9a020c964ad1c7e035f8085db4d12e88a64ab59d
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2022-03-03 11:38:20 +0000

    Create SearchResult.cs
    New model created to be used to display results of searches. Done to reduce the amount of data is loaded

---
+++ Shared/Models/SearchResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastleTours.Shared.Models
+{
+    public class SearchResult
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Rating { get; set; }
+        public string Blurb { get; set; }
+        public string Location { get; set; }
+        public string Category { get; set; }
+        public string ImgUrl { get; set; }
+        public List<Facility> Facilities { get; set; } = new List<Facility>();
+    }
+}
