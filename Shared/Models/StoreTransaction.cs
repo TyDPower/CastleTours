5320958bd8aa82b3b5eb1321fc0a8ca35ea313f7
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2022-03-03 21:56:04 +0000

    Transaction modals
    Transaction modal is the base modal with StoreTransaction modal deriving form Transaction.

---
+++ Shared/Models/StoreTransaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastleTours.Shared.Models
+{
+    public class StoreTransaction : Transaction
+    {
+        public int Id { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TicketCost { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ Shared/Models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastleTours.Shared.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int TicketId { get; set; }
+        public int TourId { get; set; }
+    }
+}
