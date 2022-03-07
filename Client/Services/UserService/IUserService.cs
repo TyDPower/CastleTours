95a2d9114e4918b5e75de5aa8756cdf9a327fc3d
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2022-03-07 10:14:40 +0000

    Update IUserService.cs

--- Client/Services/UserService/IUserService.cs
+++ Client/Services/UserService/IUserService.cs
@@ -4,8 +4,13 @@ namespace CastleTours.Client.Services.UserService
 {
     public interface IUserService
     {
+        event Action OnChange;
         public User User { get; }
         Task GetUserDetailsFromDb();
         User GetUserDetails();
+        Task<List<Favorite>> GetUserFavorites();
+        Task<Favorite> GetUserFavorite(int tourId);
+        Task<ServiceResponse<int>> AddFavorite(Favorite favorite);
+        Task<ServiceResponse<int>> RemoveFavorite(int tourId);
     }
 }
