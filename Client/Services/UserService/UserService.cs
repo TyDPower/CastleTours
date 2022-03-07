fface9a58763cb76c9040bb0ea0a63b05896ce1c
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2022-03-07 10:15:57 +0000

    Added user favorite methods
    Methods used to get a single, list of, add and remove favorites from the database

--- Client/Services/UserService/UserService.cs
+++ Client/Services/UserService/UserService.cs
@@ -7,7 +7,10 @@ namespace CastleTours.Client.Services.UserService
     public class UserService : IUserService
     {
         private readonly HttpClient Http;
-        public User User { get; private set; } = new User();
+
+        public event Action OnChange;
+
+        public User User { get; private set; }
 
         public UserService(HttpClient http)
         {
@@ -23,6 +26,30 @@ namespace CastleTours.Client.Services.UserService
         public User GetUserDetails()
         {
             return User;
+        }
+
+        public async Task<List<Favorite>> GetUserFavorites()
+        {
+            var Favorites = await Http.GetFromJsonAsync<List<Favorite>>("api/user/getUserFavorites");
+            return Favorites;
+        }
+
+        public async Task<Favorite> GetUserFavorite(int tourId)
+        {
+            var Favorite = await Http.GetFromJsonAsync<Favorite>($"api/user/getUserFavorites/{tourId}");
+            return Favorite;
+        }
+
+        public async Task<ServiceResponse<int>> AddFavorite(Favorite favorite)
+        {
+            var result = await Http.PostAsJsonAsync<Favorite>("api/auth/addFavorite", favorite);
+            return await result.Content.ReadFromJsonAsync<ServiceResponse<int>>();
+        }
+
+        public async Task<ServiceResponse<int>> RemoveFavorite(int tourId)
+        {
+            var result = await Http.DeleteAsync($"api/auth/removeFavorite/{tourId}");
+            return await result.Content.ReadFromJsonAsync<ServiceResponse<int>>();
         }
     }
 }
