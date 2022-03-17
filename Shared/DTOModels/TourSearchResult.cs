dc0a9959ac3eaf5c538c4ad79c962de87c80682d
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2022-03-17 11:27:20 +0000

    Created a data transfer object

---
+++ Shared/DTOModels/TourSearchResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CastleTours.Shared.Models;
+
+namespace CastleTours.Shared.DTOModels
+{
+    public class TourSearchResult
+    {
+        public List<SearchResult> SearchResults { get; set; }
+        public int Pages { get; set; }
+        public int CurrentPage { get; set; }
+    }
+}
