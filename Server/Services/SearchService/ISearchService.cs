2608442c418a443f98f43d00840e0d557e75ea81
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2022-03-17 11:25:50 +0000

    Search Service created

---
+++ Server/Services/SearchService/ISearchService.cs
@@ -0,0 +1,15 @@
+﻿using CastleTours.Shared.Models;
+
+namespace CastleTours.Server.Services.SearchService
+{
+    public interface ISearchService
+    {
+        Task<List<Location>> GetLocations();
+        Task<List<string>> GetLocationAreas();
+        Task<List<Facility>> GetFacilities();
+        Task<List<string>> GetFacilityTypes();
+        Task<List<Category>> GetCategories();
+        Task<List<string>> GetCategoryNames();
+        //Task<List<Sort>> GetSort();
+    }
+}
---
+++ Server/Services/SearchService/SearchService.cs
@@ -0,0 +1,69 @@
+﻿using CastleTours.Server.Data;
+using CastleTours.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CastleTours.Server.Services.SearchService
+{
+    public class SearchService : ISearchService
+    {
+        public SearchService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public DataContext _context { get; }
+
+        public async Task<List<Location>> GetLocations()
+        {
+            return await _context.Locations.ToListAsync();
+        }
+        public async Task<List<string>> GetLocationAreas()
+        {
+            var items = await _context.Locations.ToListAsync();
+            List<string> list = new List<string>();
+
+            foreach (var i in items)
+            {
+                list.Add(i.Area);
+            }
+
+            return list;
+        }
+
+        public async Task<List<Category>> GetCategories()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<List<string>> GetCategoryNames()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            List<string> list = new List<string>();
+
+            foreach (var category in categories)
+            {
+                list.Add(category.Name);
+            }
+
+            return list;
+        }
+
+        public async Task<List<Facility>> GetFacilities()
+        {
+            return await _context.Facilities.ToListAsync();
+        }
+
+        public async Task<List<string>> GetFacilityTypes()
+        {
+            var items = await _context.Facilities.ToListAsync();
+            List<string> list = new List<string>();
+
+            foreach (var i in items)
+            {
+                list.Add(i.Type);
+            }
+
+            return list;
+        }
+    }
+}
