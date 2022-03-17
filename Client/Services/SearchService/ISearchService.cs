26208400f0f6801d7eac8fba7a9d709806dcf596
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2022-03-17 11:17:15 +0000

    Created a search service to handle search and filter operations on the client side

---
+++ Client/Services/SearchService/ISearchService.cs
@@ -0,0 +1,17 @@
+﻿using CastleTours.Shared.Models;
+
+namespace CastleTours.Client.Services.SearchService
+{
+    public interface ISearchService
+    {
+        public List<Facility> Facilities { get; }
+        public List<string> FacilityTypes { get; }
+        public List<Location> Locations { get; }
+        public List<string> LocationAreas { get; }
+        public List<Category> Categories { get; }
+        public List<string> CategoryNames { get; }
+        Task LoadFacilities(string loadStrings);
+        Task LoadLocations(string loadStrings);
+        Task LoadCategories(string loadStrings);
+    }
+}
---
+++ Client/Services/SearchService/SearchService.cs
@@ -0,0 +1,58 @@
+﻿using CastleTours.Shared.Models;
+using System.Net.Http.Json;
+
+namespace CastleTours.Client.Services.SearchService
+{
+    public class SearchService : ISearchService
+    {
+        public SearchService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public HttpClient _http { get; }
+        public List<Facility> Facilities { get; private set; }
+        public List<Location> Locations { get; private set; }
+        public List<Category> Categories { get; private set; }
+        public List<string> FacilityTypes { get; private set; } = new List<string>();
+        public List<string> LocationAreas { get; private set; } = new List<string>();
+        public List<string> CategoryNames { get; private set; } = new List<string>();
+
+        public async Task LoadCategories(string loadStrings = null)
+        {
+            if (loadStrings == null || loadStrings == string.Empty)
+            {
+                Categories = await _http.GetFromJsonAsync<List<Category>>("api/search/categories");
+            }
+            else if (loadStrings == "names" || loadStrings == "name")
+            {
+                CategoryNames = await _http.GetFromJsonAsync<List<string>>("api/search/categories/names");
+            }
+            
+        }
+
+        public async Task LoadFacilities(string loadStrings = null)
+        {
+            if (loadStrings == null || loadStrings == string.Empty)
+            {
+                Facilities = await _http.GetFromJsonAsync<List<Facility>>("api/search/facilities");
+            }
+            else if (loadStrings == "types" || loadStrings == "type")
+            {
+                FacilityTypes = await _http.GetFromJsonAsync<List<string>>("api/search/facilities/types");
+            }
+        }
+
+        public async Task LoadLocations(string loadStrings = null)
+        {
+            if (loadStrings == null || loadStrings == string.Empty)
+            {
+                Locations = await _http.GetFromJsonAsync<List<Location>>("api/search/locations");
+            }
+            else if (loadStrings == "areas" || loadStrings == "area")
+            {
+                LocationAreas = await _http.GetFromJsonAsync<List<string>>("api/search/locations/areas");
+            }
+        }
+    }
+}
